cc96d9d027163569c81d324daad8d396f9b78355
Hallvard <Hallvard@158.38.79.96>
2017-09-10 14:15:22 +0200

    Added a few classes, mostly copy paste from PictureStore.

--- ChatApplication/src/main/java/no/ntnu/bjørdalsbakke/chatapplication/ConversationServices.java
+++ ChatApplication/src/main/java/no/ntnu/bjørdalsbakke/chatapplication/ConversationServices.java
@@ -7,10 +7,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import no.ntnu.bjørdalsbakke.domain.Conversation;
 import no.ntnu.bjørdalsbakke.domain.Message;
 
 /**
@@ -28,15 +30,31 @@ public class ConversationServices {
     EntityManager em;
     
     @GET
-    public List<Messages> getMessages(@QueryParam("name") String name)
+    public List<Message> getMessages(@QueryParam("name") String name)
     {
         List<Message> result = null;
         if(name != null)
         {
-            result = em.createQuery("Select m From Messaqge m"),
-            Message.Class).getResultList();
+            result = em.createQuery("Select m From Messaqge m",
+            Message.class).getResultList();
         }
         
         return result != null ? result: Collections.EMPTY_LIST;
+    }
+    
+    @POST
+    @Path("add")
+    public Message addMessage(@QueryParam("name") String name,
+            Message message)
+    {
+        System.out.println("addMessage name = " + name + "message " + message);
+        Conversation c = em.find(Conversation.class, name);
+        if (c != null)
+        {
+            message.setConversation(c);
+            em.persist(message);
+            
+        }
+        return message;
     }
 }
---
+++ ChatApplication/src/main/java/no/ntnu/bjørdalsbakke/chatapplication/RestConfig.java
@@ -0,0 +1,28 @@
+package no.ntnu.bjørdalsbakke.chatapplication;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
+/**
+ *
+ * @author mikael
+ */
+@ApplicationPath("api")
+public class RestConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(MultiPartFeature.class);
+        addRestResourceClasses(resources);
+        return resources;
+    }
+    
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(no.ntnu.bjørdalsbakke.chatapplication.ChatService.class);
+        resources.add(no.ntnu.bjørdalsbakke.chatapplication.ConversationServices.class);
+    }
+}
