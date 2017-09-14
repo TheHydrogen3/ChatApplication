5a7c376aa2fd5a854c146b5589d75a2857f94e7b
Hallvard <Hallvard@158.38.79.96>
2017-09-14 12:15:49 +0200

    fixed a compilation error in the ConversationServices class

--- ChatApplication/src/main/java/no/ntnu/bjørdalsbakke/chatapplication/ConversationServices.java
+++ ChatApplication/src/main/java/no/ntnu/bjørdalsbakke/chatapplication/ConversationServices.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import static javax.ws.rs.HttpMethod.POST;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
